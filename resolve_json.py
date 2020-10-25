cfa10e38f8abd1deb4e81d190fd95babf1dc97fd
T-yanghui <T_Yanghui@bupt.edu.cn>
2020-10-25 20:05:36 +0800

    整理文件夹

--- resolve_json.py
+++
@@ -1,6 +0,0 @@
-#解析Json文件
-import json
-jsonFile = open ('D:/code/python/ClassDemo/conf_classTime.json','r',encoding='UTF-8')
-jsonString = json.load(jsonFile)
-for name in jsonString:
-	print(name['classTime'])
